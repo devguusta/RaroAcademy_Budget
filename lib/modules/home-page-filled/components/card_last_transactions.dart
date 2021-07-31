166919271939ec335c089a98b68f5aca177c9a2e
Gustavo Silva <gustavocsil60@gmail.com>
2021-07-31 08:59:52 -0300

    adiciona listtile nas últimas transações

--- lib/modules/home-page-filled/components/card_last_transactions.dart
+++ lib/modules/home-page-filled/components/card_last_transactions.dart
@@ -8,11 +8,10 @@ class LastTransactions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Container(
         width: size.width * 0.9,
-        height: 200,
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
@@ -56,45 +55,71 @@ class LastTransactions extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 16, top: 24.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Flex(
-                    direction: Axis.horizontal,
-                    children: [
-                      Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(255, 180, 0, 1),
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        child: Icon(Icons.fastfood, color: Colors.white),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: 2,
+                    itemBuilder: (_, index) => Container(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Flex(
+                            direction: Axis.horizontal,
+                            children: [
+                              Container(
+                                width: 40,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(255, 180, 0, 1),
+                                  borderRadius: BorderRadius.circular(40),
+                                ),
+                                child:
+                                    Icon(Icons.fastfood, color: Colors.white),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 0.0, bottom: 8),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 4.0, left: 4),
+                                      child: Text("Refeição",
+                                          style: AppTextStyles
+                                              .kTitleListLastTransictions),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text("02/08/2021",
+                                          style: AppTextStyles
+                                              .kDateLastTransictions),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(width: size.width * 0.25),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      bottom: 8,
+                                    ),
+                                    child: Text("-R\$ 25,00",
+                                        style: AppTextStyles
+                                            .kValueLastTransictions),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 0.0, bottom: 8),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(top: 4.0, left: 4),
-                              child: Text("Refeição",
-                                  style:
-                                      AppTextStyles.kTitleListLastTransictions),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 8.0),
-                              child: Text("02/08/2021",
-                                  style: AppTextStyles.kDateLastTransictions),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 100, bottom: 8,),
-                        child: Text("-R\$ 25,00",
-                            style: AppTextStyles.kValueLastTransictions),
-                      ),
-                    ],
+                    ),
                   ),
                 ],
               ),
@@ -105,3 +130,5 @@ class LastTransactions extends StatelessWidget {
     );
   }
 }
+
+
