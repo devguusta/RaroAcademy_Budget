15293387abae14814f51dfc1e8a9ae4a84f16e03
Gustavo Silva <gustavocsil60@gmail.com>
2021-07-31 09:03:39 -0300

    componentiza o listview das últimas transações

--- lib/modules/home-page-filled/components/card_last_transactions.dart
+++ lib/modules/home-page-filled/components/card_last_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:raro_academy_budget/modules/home-page-filled/components/list_view_last_transactions.dart';
 import 'package:raro_academy_budget/util/constants/app_text_styles.dart';
 
 class LastTransactions extends StatelessWidget {
@@ -55,74 +56,7 @@ class LastTransactions extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 16, top: 24.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ListView.builder(
-                    shrinkWrap: true,
-                    primary: false,
-                    itemCount: 2,
-                    itemBuilder: (_, index) => Container(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Flex(
-                            direction: Axis.horizontal,
-                            children: [
-                              Container(
-                                width: 40,
-                                height: 40,
-                                decoration: BoxDecoration(
-                                  color: Color.fromRGBO(255, 180, 0, 1),
-                                  borderRadius: BorderRadius.circular(40),
-                                ),
-                                child:
-                                    Icon(Icons.fastfood, color: Colors.white),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 0.0, bottom: 8),
-                                child: Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          top: 4.0, left: 4),
-                                      child: Text("Refeição",
-                                          style: AppTextStyles
-                                              .kTitleListLastTransictions),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Text("02/08/2021",
-                                          style: AppTextStyles
-                                              .kDateLastTransictions),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              SizedBox(width: size.width * 0.25),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                      bottom: 8,
-                                    ),
-                                    child: Text("-R\$ 25,00",
-                                        style: AppTextStyles
-                                            .kValueLastTransictions),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: ListViewLastTransactions(),
             ),
           ],
         ),
@@ -130,5 +64,7 @@ class LastTransactions extends StatelessWidget {
     );
   }
 }
+
+
 
 
