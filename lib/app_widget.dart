71d13b7c792f837a1acf4cba168c7d88f7b9afca
Lucas Tamir Faria <lucastamir@gmail.com>
2021-08-07 19:56:33 -0300

    refactor: Exclusão de widget inutilizado (SplashPageSecondary)

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:raro_academy_budget/modules/home-page-filled/home_page.dart';
 import 'package:raro_academy_budget/modules/login-page/initial_login_page.dart';
-import 'package:raro_academy_budget/modules/splash-page-primary/splash_page_primary.dart';
-import 'package:raro_academy_budget/modules/splash-page-secondary/splash_page_seconday.dart';
+import 'package:raro_academy_budget/modules/splash-page/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -12,12 +11,11 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Budget',
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashPageSecondary.id,
+      initialRoute: SplashPagePrimary.id,
       routes: {
         SplashPagePrimary.id: (context) => const SplashPagePrimary(),
-        SplashPageSecondary.id: (context) => const SplashPageSecondary(),
         InitialLoginPage.id: (context) => const InitialLoginPage(),
-        HomePage.id: (context) =>  HomePage(),
+        HomePage.id: (context) => HomePage(),
         // incluir a rota nomeada como static const
         // Login.id: (context) => HomePage();
         // CreateAccount.id: (context) => HomePage();
